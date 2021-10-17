4e05dda6fbe1fab49ef6cec5b11f42390f86efd0
VBUDKE <mvbudaev@miem.hse.ru>
2021-10-17 21:35:55 +0300

    Update closest.py

--- closest.py
+++ closest.py
@@ -1,7 +1,37 @@
-def closest(array: list, target: int, count: int) -> list:
-    """Находит в упорядоченном массиве `array` `count` чисел, ближайших по значению к `target`.
-    :param array: массив, в котором идёт поиск
-    :param target: число, от которого отсчитываются ближайшие
-    :param count: количество чисел, которые необходимо выдать
-    """
-    return array
+def closest(array,target,count):
+ a=[]
+ target=int()
+ c=int(count)
+ k=0
+ p=len(array)-1
+ if (len(array)-1<c):
+  return array
+ else:
+  while(array[k]!=target)and(k!=p):
+   g=(k+p) // 2
+   d=array[g]
+   if (target>d):
+    k=g+1
+   else:
+    p=p-1
+  c=c-1
+  a.append(array[g])
+  while (c>0):
+   if (0==k):
+    p=p+1
+    a.append(array[p])
+    c=c-1
+   elif (len(array)-1==p):
+    k=k-1
+    a.insert(0,array[k])
+    print(a)
+    c=c-1
+   else:
+    c=c-1
+    if (abs(target-array[k+1])<abs(target-array[d-1])):
+     k=k-1
+     a.insert(0,array)
+    else:
+     p=p+1
+     a.append(array[p])
+  return a
